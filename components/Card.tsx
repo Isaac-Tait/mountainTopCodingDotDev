15c07a6f8865a7a49569f0102959984a945f4862
Isaac Tait <isaac@mountaintopcoding.com>
2025-01-05 08:00:55 -0800

    fix: dark mode updates 🛷

--- components/Card.tsx
+++ components/Card.tsx
@@ -29,7 +29,7 @@ const Card = ({ title, description, imgSrc, href }) => (
           />
         ))}
       <div className="p-6">
-        <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight text-purple-900">
+        <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight text-purple-900 dark:text-neutral-300">
           {href ? (
             <Link href={href} aria-label={`Link to ${title}`}>
               {title}
